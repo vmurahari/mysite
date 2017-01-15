9905b60f0c21b1db4b41bbb15b06bf8ec9ba8547
vmurahari <vmurahari@gmail.com>
2017-01-15 11:12:29 -0600

    mutler

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ const path = require('path');
 const expressValidator = require('express-validator');
 const expressStatusMonitor = require('express-status-monitor');
 //const sass = require('node-sass-middleware');
-const multer = require('multer');
+//const multer = require('multer');
 
-const upload = multer({ dest: path.join(__dirname, 'uploads') });
+//const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
